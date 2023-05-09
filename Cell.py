113768456789ee4989f966611aaa0affe712c854
Fresco2 <84515075+Fresco2@users.noreply.github.com>
2023-05-09 18:08:24 +0200

    Add files via upload

---
+++ Cell.py
@@ -0,0 +1,27 @@
+import numpy
+import cv2
+
+
+def Cell():
+    
+    def __init__(self):
+        dim = None
+        xOffset = None
+        yOffset = None
+ 
+    self.img
+    self.dim
+    self.xOffset
+    self.yOffset
+
+    def getImgCell(dimensione,x,y,game):
+        return img
+    
+    def setImgCell(frame):
+        img = numpy.zeros((dim,dim))
+        img = frame[yOffset: yOffset+dim, xOffset: xOffset+dim]
+
+
+
+        
+
